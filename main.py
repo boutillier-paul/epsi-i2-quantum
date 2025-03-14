3d498360a71cdc88bc94bca618c159a866b64564
Paul Boutillier <boutillier.paul@gmail.com>
2025-03-14 10:28:47 +0100

    push code

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import os
 from dotenv import load_dotenv
 from qiskit import QuantumCircuit
-from qiskit_ibm_runtime import QiskitRuntimeService, SamplerV2 as Sampler
+from qiskit_ibm_runtime import QiskitRuntimeService
+from qiskit_ibm_runtime import EstimatorV2 as Estimator
+from qiskit.quantum_info import SparsePauliOp
+from qiskit.transpiler.preset_passmanagers import generate_preset_pass_manager
+
 
 load_dotenv()
  
@@ -11,12 +15,37 @@ QiskitRuntimeService.save_account(
   overwrite=True
 )
 
-example_circuit = QuantumCircuit(2)
-example_circuit.measure_all()
- 
 service = QiskitRuntimeService()
-backend = service.least_busy(operational=True, simulator=False)
+
+# Create a new circuit with two qubits
+qc = QuantumCircuit(2)
  
-sampler = Sampler(backend)
-job = sampler.run([example_circuit])
-print(f"job id: {job.job_id()}")
+# Add a Hadamard gate to qubit 0
+qc.h(0)
+ 
+# Perform a controlled-X gate on qubit 1, controlled by qubit 0
+qc.cx(0, 1)
+
+# Set up six different observables.
+observables_labels = ["IZ", "IX", "ZI", "XI", "ZZ", "XX"]
+observables = [SparsePauliOp(label) for label in observables_labels]
+ 
+backend = service.least_busy(simulator=False, operational=True)
+
+# Convert to an ISA circuit and layout-mapped observables.
+pm = generate_preset_pass_manager(backend=backend, optimization_level=1)
+isa_circuit = pm.run(qc)
+ 
+estimator = Estimator(mode=backend)
+estimator.options.resilience_level = 1
+estimator.options.default_shots = 5000
+ 
+mapped_observables = [
+    observable.apply_layout(isa_circuit.layout) for observable in observables
+]
+ 
+# One pub, with one circuit to run against five different observables.
+job = estimator.run([(isa_circuit, mapped_observables)])
+ 
+# Use the job ID to retrieve your job data later
+print(f">>> Job ID: {job.job_id()}")
